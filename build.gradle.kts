05f8ae101b63e608d0029594475d42d6d08d9175
cristianberti <cristiandalloca@gmail.com>
2025-05-07 21:23:06 -0300

    Update dependency versions for SpringDoc and Spring Modulith
    Bumped SpringDoc OpenAPI from 2.8.6 to 2.8.8 and Spring Modulith from 1.3.4 to 1.3.5. This ensures compatibility with the latest features and fixes provided by these libraries.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-val springDocOpenApiVersion = "2.8.6"
+val springDocOpenApiVersion = "2.8.8"
 val testContainersVersion = "1.21.0"
-val springModulithVersion = "1.3.4"
+val springModulithVersion = "1.3.5"
 val awaitilityVersion = "4.3.0"
 
 plugins {
